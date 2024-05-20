ead4065ec61d605c3d146112c594ead53a22d7a8
yiw107 <1458286231@qq.com>
2024-05-20 19:16:20 +0800

    删除功能实现

--- src/App.vue
+++ src/App.vue
@@ -14,8 +14,13 @@ const getlist = async()=>{
 }
 
 onMounted(()=>getlist())//在onMounted中调用getlist方法
-// TODO: 删除功能
-
+// TODO: 删除功能 思路：获取当前行的id→通过id调用删除接口→更新最新的列表
+//1.准备一个删除的回调函数
+const onDelete = async(id) =>{
+  
+  await axios.delete(`/del/${id}`)
+  getlist()
+}
 
 // TODO: 编辑功能
 
@@ -28,9 +33,9 @@ onMounted(()=>getlist())//在onMounted中调用getlist方法
       <el-table-column label="姓名" prop="name" width="150"></el-table-column>
       <el-table-column label="籍贯" prop="place"></el-table-column>
       <el-table-column label="操作" width="150">
-        <template #default>
+        <template #default="{row}">
           <el-button type="primary" link>编辑</el-button>
-          <el-button type="danger" link>删除</el-button>
+          <el-button type="danger" @click="onDelete(row.id)" link>删除</el-button>
         </template>
       </el-table-column>
     </el-table>
